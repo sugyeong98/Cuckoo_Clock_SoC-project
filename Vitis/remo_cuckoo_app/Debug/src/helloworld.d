55152f6a86110366f92ee21a957099cc9e7639f8
SeoDongYoon <hama510@naver.com>
2024-11-13 14:41:53 +0900

    Vitis 최종본 코드

---
+++ Vitis/remo_cuckoo_app/Debug/src/helloworld.d
@@ -0,0 +1,57 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_printf.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_types.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xparameters.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/bspconfig.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xparameters.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xiic.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_assert.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xstatus.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xiic_l.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_io.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_printf.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/mb_interface.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_exception.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/sleep.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xuartlite.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xgpio.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xgpio_l.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_printf.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_types.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/bspconfig.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xiic.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_assert.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xstatus.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xiic_l.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_io.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_printf.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/mb_interface.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_exception.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/sleep.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xuartlite.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xgpio.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xgpio_l.h:
---
+++ Vitis/remo_cuckoo_app/Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xparameters.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_cache.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/mb_interface.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_types.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_assert.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_exception.h \
+ C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xparameters.h \
+ ../src/platform_config.h
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_cache.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/mb_interface.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_types.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_assert.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xil_exception.h:
+
+C:/soc_workspace/Soc_soc_24_1/remo_cuckoo_hw_v2/export/remo_cuckoo_hw_v2/sw/remo_cuckoo_hw_v2/standalone_domain/bspinclude/include/xparameters.h:
+
+../src/platform_config.h:
